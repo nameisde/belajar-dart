3f1cb87794178dba67ffa99b39c478e7bb563287
nameisde <nameisde@gmail.com>
2023-02-23 19:31:18 +0700

    Lesson 1 - variables

---
+++ javascript/lesson/1-variables.js
@@ -0,0 +1,19 @@
+// Variabel adalah wadah untuk menyimpan data
+// Variabel berperilaku seolah-olah itu adalah nilai yang dikandungnya
+
+// Tahapan:
+// 1. Declaration (var, let, const)
+// 2. Assigment (= assignment operator)
+
+// variable dengan value numbers
+let age = 24;
+
+// variable dengan value strings
+let firstName = "Dee";
+
+// variable dengan value booleans
+let student = false;
+
+console.log(age);
+console.log(firstName);
+console.log(student);
